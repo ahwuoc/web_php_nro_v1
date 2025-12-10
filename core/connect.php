f87560297212010d9ba5e173fefa06cd90afbc61
ahwuocdz <ahwuocdz@gmail.com>
2025-12-10 11:42:46 +0700

    fix(connect.php) disable SSL in connect.php to prevent connection failure

--- core/connect.php
+++ core/connect.php
@@ -4,17 +4,14 @@ $dbname_sv = "nro";
 $user_sv = "ahwuocdz";
 $pass_sv = "123456";
 
-// GMT +7
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 
 try {
-    // Create connection
-    $dsn = "mysql:host=$ip_sv;dbname=$dbname_sv;charset=utf8mb4";
-    $conn = new PDO($dsn, $user_sv, $pass_sv);
-
-    // Set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=$ip_sv;dbname=$dbname_sv;charset=utf8mb4;sslmode=disabled";
+    $conn = new PDO($dsn, $user_sv, $pass_sv, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
 } catch (PDOException $e) {
-    die("Connection failed");
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
